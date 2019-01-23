09284324eff8dd9edd5af314aa6db53c7e96331a
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-01-23 12:23:58 -0600

    Includes of examples of repetition using recursion and loop/recur.

--- src/0. Introduction/simple.clj
+++ src/0. Introduction/simple.clj
@@ -19,6 +19,48 @@
       1
       0)))
 
+(defn roots
+  "Returns a vector containing the two possible roots
+  that solve a quadratic equation given its three
+  coefficients a, b, and c."
+  [a b c]
+  (let [d (- b)
+        e (sqrt (- (expt b 2)
+                   (* 4 a c)))
+        f (* 2 a)
+        x1 (/ (+ d e) f)
+        x2 (/ (- d e) f)]
+    [x1 x2]))
+
+(defn bmi
+  "Returns the body mass index of person with a certain
+  weight (in kilograms) and height (in meters)."
+  [weight height]
+  (let [BMI (/ weight (expt height 2))]
+    (cond
+      (< BMI 20) 'underweight
+      (< BMI 25) 'normal
+      (< BMI 30) 'obese1
+      (< BMI 40) 'obese2
+      :else      'obese3)))
+
+(defn !
+  "Returns the factorial of n. Uses recursion."
+  [n]
+  (if (zero? n)
+    1
+    (*' n (! (dec n)))))
+
+(defn !2
+  "Returns the factorial of n. Uses loop/recur."
+  [n]
+  (loop [i      1
+         result 1]
+    (if (> i n)
+      result
+      (recur (inc i)
+             (*' result i)))))
+
 (deftest test-f2c
   (is (= 100.0 (f2c 212.0)))
   (is (= 0.0 (f2c 32.0)))
@@ -28,5 +70,17 @@
   (is (= -1 (sign -5)))
   (is (= 1 (sign 10)))
   (is (= 0 (sign 0))))
+
+(deftest test-roots
+  (is (= [-1 -1] (roots 2 4 2)))
+  (is (= [0 0] (roots 1 0 0)))
+  (is (= [-1/4 -1] (roots 4 5 1))))
+
+(deftest test-bmi
+  (is (= 'underweight (bmi 45 1.7)))
+  (is (= 'normal (bmi 55 1.5)))
+  (is (= 'obese1 (bmi 76 1.7)))
+  (is (= 'obese2 (bmi 81 1.6)))
+  (is (= 'obese3 (bmi 120 1.6))))
 
 (run-tests)
