d7f4e000d0b80cbdc41e8b54c6972fec0ea342f8
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-02-01 15:58:54 -0600

    Examples using the Fibonacci sequence.

--- src/1. Recursion and Sequence API/examples.clj
+++ src/1. Recursion and Sequence API/examples.clj
@@ -25,3 +25,35 @@
   each appearing twice. Uses the sequence API."
   [lst]
   (mapcat #(list % %) lst))
+
+(defn fib1
+  "Returns the n-th element of the Fibonacci sequence.
+  Assumes that n is an integer greator or equal to zero.
+  Uses recursion."
+  [n]
+  (if (< n 2)
+    n
+    (+' (fib1 (- n 1))
+        (fib1 (- n 2)))))
+
+(defn fib2
+  "Returns the n-th element of the Fibonacci sequence.
+  Assumes that n is an integer greator or equal to zero.
+  Uses loop/recur."
+  [n]
+  (loop [a 0
+         b 1
+         i 0]
+    (if (= i n)
+      a
+      (recur b
+             (+' a b)
+             (inc i)))))
+
+(defn fib3
+  "Returns the n-th element of the Fibonacci sequence.
+  Assumes that n is an integer greator or equal to zero.
+  Uses the sequence API."
+  [n]
+  ;;; To be defined
+  nil)
