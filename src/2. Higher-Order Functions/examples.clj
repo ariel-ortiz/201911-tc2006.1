dac9e78b7b6a8df987719f49ce16bf5ad43bba26
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-02-12 15:55:46 -0600

    Implementation of power-set function.

--- src/2. Higher-Order Functions/examples.clj
+++ src/2. Higher-Order Functions/examples.clj
@@ -16,3 +16,14 @@
 (def f4 (compose f2 f1))
 
 (def f5 (compose f3 f4))
+
+
+(defn power-set
+  "Returns a list with all the
+  possible subsets of s."
+  [s]
+  (if (empty? s)
+    '(())
+    (let [rec-result (power-set (rest s))]
+      (concat rec-result
+              (map #(cons (first s) %) rec-result)))))
