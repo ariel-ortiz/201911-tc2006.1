4147ebf9025a09e538418ee53e0b6a66e97b6b3d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-03-05 14:37:23 -0600

    Impementation of the multiplication operator.

--- src/3. Symbolic Differentiation/derivative.clj
+++ src/3. Symbolic Differentiation/derivative.clj
@@ -3,15 +3,15 @@
 ;;;    http://34.212.143.74/s201911/tc2006/notes/symbolic_differentiation.html
 
 (declare variable?
-         same-variable?
-         sum?
-         augend
-         addend
-         make-sum
-         product?
-         multiplicand
-         multiplier
-         make-product)
+  same-variable?
+  sum?
+  augend
+  addend
+  make-sum
+  product?
+  multiplicand
+  multiplier
+  make-product)
 
 (defn deriv
   "Derives exp with respect to var."
@@ -28,7 +28,16 @@
 
     (sum? exp)
     (make-sum (deriv (augend exp) var)
-              (deriv (addend exp) var))))
+      (deriv (addend exp) var))
+
+    (product? exp)
+    (make-sum (make-product (multiplicand exp)
+                            (deriv (multiplier exp) var))
+              (make-product (multiplier exp)
+                            (deriv (multiplicand exp) var)))
+    :else
+    (throw (RuntimeException.
+             (str "Unrecognized expresion: " exp)))))
 
 (defn variable?
   "Is exp a variable?"
@@ -39,15 +48,15 @@
   "Are v1 and v2 the same variable?"
   [v1 v2]
   (and (variable? v1)
-       (variable? v2)
-       (= v1 v2)))
+    (variable? v2)
+    (= v1 v2)))
 
 (defn sum?
   "Is exp a sum?"
   [exp]
   (and (list? exp)
-       (= 3 (count exp))
-       (= '+ (first exp))))
+    (= 3 (count exp))
+    (= '+ (first exp))))
 
 (defn augend
   "Returns the augend of a sum."
@@ -62,4 +71,55 @@
 (defn make-sum
   "Construct the sum of a1 and a2."
   [a1 a2]
-  (list '+ a1 a2))
+  (cond
+    (= 0 a1)
+    a2
+
+    (= 0 a2)
+    a1
+
+    (and (number? a1) (number? a2))
+    (+ a1 a2)
+
+    :else
+    (list '+ a1 a2)))
+
+(defn product?
+  "Is exp a product?"
+  [exp]
+  (and (list? exp)
+    (= 3 (count exp))
+    (= '* (first exp))))
+
+(defn multiplicand
+  "Multiplicand of the product exp."
+  [exp]
+  (nth exp 1))
+
+(defn multiplier
+  "Multiplier of the product exp."
+  [exp]
+  (nth exp 2))
+
+(defn make-product
+  "Construct the product of m1 and m2."
+  [m1 m2]
+  (cond
+
+    (= 0 m1)
+    0
+
+    (= 0 m2)
+    0
+
+    (= 1 m1)
+    m2
+
+    (= 1 m2)
+    m1
+
+    (and (number? m1) (number? m2))
+    (* m1 m2)
+
+    :else
+    (list '* m1 m2)))
