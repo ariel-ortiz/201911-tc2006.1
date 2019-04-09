2e29263d761fdca12efb4153f37a97674ad9193f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-04-09 16:01:01 -0500

    First logic relations using the GOT database.

--- src/7. miniKanren/gotdb.clj
+++ src/7. miniKanren/gotdb.clj
@@ -8,17 +8,40 @@
 (def got-db
   (pldb/db
     ;;;        name      gender  father   mother
-    [character :eddard   :male   :rickard :lyarra ]
-    [character :brandon  :male   :rickard :lyarra ]
-    [character :lyanna   :female :rickard :lyarra ]
-    [character :benjen   :male   :rickard :lyarra ]
+    [character :eddard   :male   :rickard :lyarra]
+    [character :brandon  :male   :rickard :lyarra]
+    [character :lyanna   :female :rickard :lyarra]
+    [character :benjen   :male   :rickard :lyarra]
     [character :robb     :male   :eddard  :catelyn]
     [character :sansa    :female :eddard  :catelyn]
     [character :arya     :female :eddard  :catelyn]
     [character :bran     :male   :eddard  :catelyn]
     [character :rickon   :male   :eddard  :catelyn]
-    [character :jon      :male   :rhaegar :lyanna ]
+    [character :jon      :male   :rhaegar :lyanna]
     [character :rhaegar  :male   :aerys   :rhaella]
     [character :elia     :female :aerys   :rhaella]
     [character :viserys  :male   :aerys   :rhaella]
     [character :daenerys :female :aerys   :rhaella]))
+
+(defn mothero
+  "Logical function that unifies if m is the mother of p."
+  [m p]
+  (logic/fresh [_g _f]
+    (character p _g _f m)))
+
+(defn fathero
+  "Logical function that unifies if f is the father of p."
+  [f p]
+  (logic/fresh [_g _m]
+    (character p _g f _m)))
+
+(defn grandfathero
+  "Logical function that unifies if gf is the grandfather of p."
+  [gf p]
+  (logic/fresh [_x]
+    (logic/conde
+      [(fathero gf _x)
+       (fathero _x p)]
+      [(fathero gf _x)
+       (mothero _x p)])))
+
