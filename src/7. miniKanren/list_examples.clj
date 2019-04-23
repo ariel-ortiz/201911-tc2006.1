2c8ce1772eb9be5a4ee5bc24ee6fe8e3bec66620
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-04-23 16:01:59 -0500

    More examples using miniKanren with lists.

--- src/7. miniKanren/list_examples.clj
+++ src/7. miniKanren/list_examples.clj
@@ -10,8 +10,39 @@
 
 (logic/defne dupo
   "Logical function that succeeds if every element of
-  lst is duplicated in result."
+  lst is duplicates in result."
   [lst result]
   ([[] []])
   ([[head . tail] [head head . temp]]
    (dupo tail temp)))
+
+(logic/defne reverseo
+  "Logical function that succeeds if the reverse of
+  lst is result."
+  [lst result]
+  ([[] []])
+  ([[head . tail] result]
+   (logic/fresh [temp]
+     (logic/appendo temp [head] result)
+     (reverseo tail temp))))
+
+(logic/defne twino
+             "Logical function that succeeds if lst is a sequence
+             of two equal items."
+  [lst]
+  ([[x x]]))
+
+(logic/defne anti-twino
+  "Logical function that succeeds if lst is a sequence
+  of two items that are not equal."
+  [lst]
+  ([[x y]]
+   (logic/!= x y)))
+
+(logic/defne listo
+  "Logical function that succeeds if each element of lst
+   is contained within its own list inside result."
+  [lst result]
+  ([[] []])
+  ([[head . tail] [[head] . temp]]
+   (listo tail temp)))
